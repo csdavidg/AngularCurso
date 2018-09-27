06eaca7bae3582571562b56c1d78f672edac54b9
David <csdavidg@hotmail.com>
2018-09-26 22:27:00 -0500

    Inicio PIPES

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  nombre = "Cristian";
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+}
